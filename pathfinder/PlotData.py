897c9d7561276000ea6ea84188952c32e6a1a6f8
davis67 <davisag67@gmail.com>
2019-05-27 10:41:59 +0300

    Created the geo mapping component

--- pathfinder/PlotData.py
+++ pathfinder/PlotData.py
@@ -4,7 +4,7 @@ from bokeh.plotting import gmap
 from .model import CrimeScene
 from bokeh.io import output_file, output_notebook, show, save
 from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, LogColorMapper, BasicTicker, ColorBar,
+    GMapPlot, GMapOptions, ColumnDataSource, Circle, LogColorMapper, BasicTicker, ColorBar,
     Range1d, PanTool, WheelZoomTool, BoxSelectTool
 )
 from bokeh.models.mappers import ColorMapper, LinearColorMapper
@@ -13,30 +13,30 @@ import os
 
 
 def get_co_ordinates():
-	crimes_scenes = CrimeScene.query.all()
-	longitudes = [ crimescene.longitude for crimescene in crimes_scenes ]
-	latitudes = [ crimescene.latitude for crimescene in crimes_scenes ]
-	return latitudes,longitudes
-
-
+    crimes_scenes = CrimeScene.query.all()
+    longitudes = [crimescene.longitude for crimescene in crimes_scenes]
+    latitudes = [crimescene.latitude for crimescene in crimes_scenes]
+    return latitudes, longitudes
 
 
 def showmap():
-    map_options = GMapOptions(lat=0.3132008, lng=32.5290843, map_type="roadmap", zoom=11)
+    map_options = GMapOptions(
+        lat=0.3132008, lng=32.5290843, map_type="roadmap", zoom=11)
     GOOGLE_API_KEY = "AIzaSyAFCR-n7VxtftzPKR4gCje1T-cAxQXn7S8"
     plot = gmap(GOOGLE_API_KEY, map_options, title="Crimes visualizing center")
-    latitude_list,longitude_list = get_co_ordinates()
+    latitude_list, longitude_list = get_co_ordinates()
+    print(latitude_list, longitude_list)
     source = ColumnDataSource(
-           data=dict(
-	       lat=latitude_list,
-               lon=longitude_list
-           )
+        data=dict(
+            lat=latitude_list,
+            lon=longitude_list
+        )
     )
-    plot.circle(x="lat", y="lon", size=15, fill_color="blue", fill_alpha=0.8, source=source)
+    plot.circle(x="lon", y="lat", size=15, fill_color="green",
+                fill_alpha=0.8, source=source)
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     storage_path = os.path.join(BASE_DIR, 'pathfinder/templates/pages')
     map_path = os.path.join(storage_path, 'gmap_plot.html')
     output_file(map_path)
     print("saved...")
     save(plot)
-
--- pathfinder/home/views.py
+++ pathfinder/home/views.py
@@ -1,13 +1,13 @@
 from flask import flash, abort, render_template, jsonify
 from flask_login import current_user, login_required
-from .forms import LoginForm, AddPoliceStationForm,RegistrationForm
+from .forms import LoginForm, AddPoliceStationForm, RegistrationForm
 from ..model import Police, User, Category, CrimeScene
 from .. import db
 from . import home
 from ..PlotData import showmap
 
 
-@home.route('/',methods=['GET', 'POST'])
+@home.route('/', methods=['GET', 'POST'])
 def homepage():
     """
     Render the home page on / route
@@ -16,17 +16,18 @@ def homepage():
 
     # policestations = Police.query.all()
     return render_template('home/index.html', title="welcome",
-                             form=form, methods=['GET', 'POST'])
+                           form=form, methods=['GET', 'POST'])
 
-#add admin dashboard view
+# add admin dashboard view
 @home.route('/admin/dashboard')
-@login_required
+# @login_required
 def admin_dashboard():
-    #prevent non-admins from accessing the page
-    if not current_user.is_admin:
-        abort(403)
+    # prevent non-admins from accessing the page
+    # if not current_user.is_admin:
+    #     abort(403)
     showmap()
     return render_template('home/admin_dashboard.html', title="dashboard")
+
 
 @home.route('/admin/policestation', methods=['POST', 'GET'])
 # @login_required
@@ -35,16 +36,16 @@ def police_station_data():
     #     abort(403)
     form = AddPoliceStationForm()
     if form.validate_on_submit():
-        police_station = Police(StationName = form.StationName.data,
-                                division = form.division.data)
+        police_station = Police(StationName=form.StationName.data,
+                                division=form.division.data)
         db.session.add(police_station)
         db.session.commit()
         flash("You have successfully added a Police Station.")
     police_stations = Police.query.all()
     return render_template('home/police_index.html',
-                            title="PoliceStation",
-                            police_stations=police_stations,
-                            form=form)
+                           title="PoliceStation",
+                           police_stations=police_stations,
+                           form=form)
 
 
 @home.route('/admin/register_user', methods=['GET', 'POST'])
@@ -56,19 +57,20 @@ def register_user():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(
-                    email = form.email.data,
-                    full_names= form.full_names.data,
-                    telephone =form.telephone.data,
-                    password = form.password.data
-                    )
-        #add employee to the database
+            email=form.email.data,
+            full_names=form.full_names.data,
+            telephone=form.telephone.data,
+            password=form.password.data
+        )
+        # add employee to the database
         db.session.add(user)
         db.session.commit()
         flash('You have successively registered! You may now login.')
 
-    #redirect to the login page
+    # redirect to the login page
     return render_template('home/register.html', title="Register User",
-                             form=form)
+                           form=form)
+
 
 @home.route('/web/PathFinder/v1.0/data/to-day')
 def collect_data():
@@ -76,30 +78,30 @@ def collect_data():
     # today = datetime.datetime.strftime(datetime.datetime.today(),datemask)
     # today_cases = CaseFile.query.filter_by(date_posted = today)
 
-    labels,actual_data = category_crimes_data()
+    labels, actual_data = category_crimes_data()
     crimesdata = {
-        'labels':labels,
+        'labels': labels,
         'data': actual_data
     }
     print('request made.............\n ')
-    return jsonify({'crimesdata':crimesdata})
+    return jsonify({'crimesdata': crimesdata})
+
 
 @home.route('/web/PathFinder/v1.0/data/')
 def collect_summary_data():
     casesData = {
-        'thefty':len(Category.query.filter_by(violet_type = 'thefty').first().crimescene),
-        'murder':len(Category.query.filter_by(violet_type = 'murder').first().crimescene),
-        'kidnap':len(Category.query.filter_by(violet_type = 'kidnap').first().crimescene),
-        'robbery':len(Category.query.filter_by(violet_type = 'robbery').first().crimescene)
+        'thefty': len(Category.query.filter_by(violet_type='Thefty').first().crimescene),
+        'murder': len(Category.query.filter_by(violet_type='murder').first().crimescene),
+        'kidnap': len(Category.query.filter_by(violet_type='kidnap').first().crimescene),
+        'robbery': len(Category.query.filter_by(violet_type='robbery').first().crimescene)
     }
-    return jsonify({'casesData':casesData})
-
-
+    return jsonify({'casesData': casesData})
 
     """
     returns the taple of categories list the count of crimes count
     list
     """
+
 
 def category_crimes_data():
     crime_categories = []
